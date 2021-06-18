d72ae97f108d7c91485f323c507fc123f7f5b637
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-06-18 12:47:45 +0200

    fix various compile errors

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -29,4 +29,6 @@ pub use ssi::vc::LinkedDataProofOptions;
 pub use ssi::vc::Presentation as VerifiablePresentation;
 pub use ssi::vc::ProofPurpose;
 pub use ssi::vc::VerificationResult;
+pub use ssi::vc::URI;
 pub use ssi::zcap::Delegation;
+pub use ssi::zcap::Invocation;
